41aec30fc88737ea454642c989fed16f87259199
lucasalisboa <43450181+lucasalisboa@users.noreply.github.com>
2019-06-18 17:48:21 -0300

    Update Strassen.c

--- Strassen.c
+++ Strassen.c
@@ -29,6 +29,66 @@ void print_result(int** result,int n1, int m2){
 
     }
 	fclose(out);
+} 
+
+int** check(int** matriz, int n)
+{
+	if(n == 1)
+	{
+		return matriz;
+	}
+	else
+	{
+		if((n % 2) == 0)
+		{
+			check(matriz, (n/2));
+		}
+		else
+		{
+			
+		}
+	}
+}
+
+int** strassen_multi(int** matriz_a,int** matriz_b,int n1,int m1, int n2, int m2)
+{
+	if(n1 == 1 && m1 == 1 && n2 == 1 && m2 == 1)
+	{
+		int result[1][1];
+		result[0][0] = (matriz_a[0][0]*matriz_b[0][0]);
+		return result;
+	}
+	else if(n1 == m1 && n2 == m2)
+	{
+
+	}
+	/*
+		int s1 = matriz_b[0][1] - matriz_b[1][1];
+		int s2 = matriz_a[0][0] + matriz_a[0][1];
+		int s3 = matriz_a[1][0] + matriz_a[1][1];
+		int s4 = matriz_b[1][0] - matriz_b[0][0];
+		int s5 = matriz_a[0][0] + matriz_a[1][1];
+		int s6 = matriz_b[0][0] + matriz_b[1][1];
+		int s7 = matriz_a[0][1] - matriz_a[1][1];
+		int s8 = matriz_b[1][0] + matriz_a[1][1];
+		int s9 = matriz_a[0][0] - matriz_a[1][0];
+		int s10 = matriz_b[0][0] + matriz_b[0][1];
+
+		int p1 = s1 * matriz_a[0][0];
+		int p2 = s2 * matriz_b[1][1];
+		int p3 = s3 * matriz_b[0][0];
+		int p4 = s4 * matriz_a[1][1];
+		int p5 = s5 * s6;
+		int p6 = s7 * s8;
+		int p7 = s9 * s10;
+
+		int c[2][2];
+		c[0][0] = p5 + p4 -p2 + p6;
+		c[0][1] = p1 + p2;
+		c[1][0] = p3 + p4;
+		c[1][1] = p5 + p1 - p3 - p7;
+	*/
+
 }
 
 int main()
@@ -82,7 +142,7 @@ int main()
       aux_2++;
   }
 
-  int result [n1][m2] = strassen_multi(matriz_a,matriz_b);
+  int result [n1][m2] = strassen_multi(matriz_a,matriz_b,n1,m1,n2,m2);
   print_result(result,n1,m2);
 
   fclose(in);
